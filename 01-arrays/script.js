304d6d4e955a470db4bc2f226dc29c2318075a78
Sara-Fioretti <fiorettisara22@gmail.com>
2026-02-03 22:29:30 +0100

    ES 1.5

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -27,6 +27,8 @@ const firstTeacher = teachers.splice (0,1)
 console.log (firstTeacher)
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
+teachers.push("Vanessa")
+console.log(teachers)
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
 
