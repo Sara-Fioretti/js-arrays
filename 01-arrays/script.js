e1e142af9e464b999928d4efbee37998bf88e120
Sara-Fioretti <fiorettisara22@gmail.com>
2026-02-03 22:26:44 +0100

    ES 1.4

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -23,7 +23,8 @@ console.log(lastTeacher)
 
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+const firstTeacher = teachers.splice (0,1)
+console.log (firstTeacher)
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 
