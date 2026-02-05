706bea5183f6eb953d7187cb70e8d1f90e7bd33a
Sara-Fioretti <fiorettisara22@gmail.com>
2026-02-05 10:01:31 +0100

    ES 2.3

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -18,6 +18,10 @@ console.log(reversedTeachers)
 const longNames = null;
 
 // 3. Rimuovi 'Ed' dall'array teachers
+indexOfEd = teachers.indexOf("Ed")
+console.log(indexOfEd)
+teachers.splice(5,1)
+console.log(teachers)
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
