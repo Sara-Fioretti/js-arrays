919cb071a236fc0dd974549277432649e94fbf56
Sara-Fioretti <fiorettisara22@gmail.com>
2026-02-05 10:40:26 +0100

    ES 2.4

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -31,8 +31,9 @@ console.log(teachers)
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent= null; 
-
+console.log(teachers.indexOf("Fabio"))
+const isFabioPresent= "True"; 
+console.log(isFabioPresent)
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
 const teachersString = teachers.toString();
